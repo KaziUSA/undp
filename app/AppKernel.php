ac3046e5c020dedd6abca9f86139ce87c57c997f
Nikesh <nikesh@kazistudios.com>
2016-01-14 16:45:51 +0545

    Added contact bundle to test.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new KaziBundle\KaziBundle(),
             new Phax\CoreBundle\PhaxCoreBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new Mremi\ContactBundle\MremiContactBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
