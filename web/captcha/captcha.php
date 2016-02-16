171e3cac450c4dba229ca28e4f8e60d296e9fe9d
Nikesh <nikesh@kazistudios.com>
2016-02-16 13:40:11 +0545

    Captcha font changed and size increased.

--- web/captcha/captcha.php
+++ web/captcha/captcha.php
@@ -14,7 +14,12 @@ $_SESSION["code"] = $captchanumber;
 //Generating CAPTCHA
 $image = imagecreatefromjpeg("bj.jpg");
 $foreground = imagecolorallocate($image, 153, 188, 68); //font color
-imagestring($image, 5, 45, 8, $captchanumber, $foreground);
+
+//imagestring($image, 5, 45, 8, $captchanumber, $foreground);
+$color = imagecolorallocate($image, 153, 188, 68);//black - 0, 0, 0
+$font = 'WWF.otf';
+imagettftext($image, 20, 0, 45, 25, $color, $font, $captchanumber);
+
 header('Content-type: image/png');
 imagepng($image);
 ?>
